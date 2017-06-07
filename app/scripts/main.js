a5f77be195694194c5812c30e0f3351a3099094b
Talha <talha@marketlytics.com>
2017-06-07 13:51:31 +0500

    Initialise project

---
+++ app/scripts/main.js
@@ -0,0 +1,5 @@
+console.log('\'Allo \'Allo!');
+var $ = require('../../bower_components/jquery/dist/jquery.js');
+$(document).ready(function(){
+    $('body').html('Hello world!');
+});
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entry: './app/scripts/main.js',
+  output: {
+    filename: './app/scripts/bundle.js'
+  },
+  watch: true
+};
